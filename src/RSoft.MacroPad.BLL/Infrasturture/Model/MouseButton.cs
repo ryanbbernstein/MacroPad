3063504a84683ebd2161597d38a53bde336fd396
Ryan Bernstein <ryanbbernstein@gmail.com>
2025-04-04 02:40:42 -0700

    Fixing MouseButtonMapper by adding VirtualKeyMaps to the MouseButtons

--- src/RSoft.MacroPad.BLL/Infrasturture/Model/MouseButton.cs
+++ src/RSoft.MacroPad.BLL/Infrasturture/Model/MouseButton.cs
@@ -3,18 +3,23 @@
     public enum MouseButton
     {
         [MouseValues(1,0)]
+        [VirtualKeyMap(VirtualKey.LButton)]
         Left,
 
         [MouseValues(4, 0)]
+        [VirtualKeyMap(VirtualKey.MButton)]
         Middle,
 
         [MouseValues(2, 0)]
+        [VirtualKeyMap(VirtualKey.RButton)]
         Right,
 
         [MouseValues(0, 1)]
+        [VirtualKeyMap(VirtualKey.Up)]
         ScrollUp,
 
         [MouseValues(0, 255)]
+        [VirtualKeyMap(VirtualKey.Down)]
         ScrollDown
     }
 }
